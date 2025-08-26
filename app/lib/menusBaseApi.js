ac64774be3f1b40ed79e8cc84dd87eeba15aeddd
Ali Hanafiah <hanafiaha308@gmail.com>
2025-08-26 11:20:19 +0700

    roll back commit 11: 20 Fix 502 login

--- app/lib/menusBaseApi.js
+++ app/lib/menusBaseApi.js
@@ -5,9 +5,5 @@ const base = '/api/menus';
 export const menusbApi = {
     list(opts) {
         return request(`${base}`, { method: 'GET', cache: 'no-store', ...(opts || {}) });
-    },
-    getSidebarMenu(opts) {
-        return request(`${base}/all`, { method: 'GET', cache: 'no-store', ...(opts || {}) });
     }
-
 }
